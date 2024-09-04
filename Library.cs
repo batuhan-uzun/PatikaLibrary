67a16aeb468b7727738e5ce74826cabf495837ab
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-04 04:23:59 +0300

    Add project files.

---
+++ Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaLibrary
+{
+    internal class Library // Class: Contains properties and behavior of an object. Veriables, methods or constructors containing methods can be defined inside classes
+    {
+        // Constructor: A special method that is automatically invoked when creating a new object of a class. It can be used by default, but it also allows us to perform different operations with alternative constructors
+        public Library() // Default Constructor with no parameters
+        {
+            RegistrationDate = DateTime.Now;
+        }
+
+        public Library(string bookName, string writerName, string writerSurname, int pageCount, string publisher) // Alternative Constructor that takes parameters
+        {
+            RegistrationDate = DateTime.Now;
+            BookName = bookName;
+            WriterName = writerName;
+            WriterSurname = writerSurname;
+            PageCount = pageCount;
+            Publisher = publisher;
+        }
+
+        // Property: Objects that store the data of a class and determine the type of access. Properties are defined as get set
+        public string BookName { get; set; } // We get set the book name,writer name and surname, page count, publisher and registration date properties
+        public string WriterName { get; set; }
+        public string WriterSurname { get; set; }
+        public int PageCount { get; set; }
+        public string Publisher { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿using PatikaLibrary;
+
+Library library1 = new Library() // new: Creates a new object in memory using the properties contained in a class.
+{
+    BookName = "Bir Kitap",
+    WriterName = "Ahmet",
+    WriterSurname = "Yılmaz",
+    PageCount = 320,
+    Publisher = "Alfa Yayınları"
+};
+
+Console.WriteLine($"Kitap Adı: {library1.BookName} Yazar Adı: {library1.WriterName} Yazar Soyadı: {library1.WriterSurname} Sayfa Sayısı: {library1.PageCount} Yayınevi: {library1.Publisher} Kayıt Tarihi: {library1.RegistrationDate.ToString("yyyy-MM-dd")}"); // We print first object created with default constructor
+
+Console.WriteLine("Kitabın adını giriniz");
+string bookName = Console.ReadLine();
+Console.WriteLine("Yazarın adını giriniz");
+string writerName = Console.ReadLine();
+Console.WriteLine("Yazarın soyadını giriniz");
+string writerSurname = Console.ReadLine();
+Console.WriteLine("Sayfa sayısını giriniz");
+int pageCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Yayınevini giriniz");
+string publisher = Console.ReadLine();
+
+Library library2 = new Library(bookName, writerName, writerSurname, pageCount, publisher);
+Console.WriteLine($"Kitap Adı: {library2.BookName} Yazar Adı: {library2.WriterName} Yazar Soyadı: {library2.WriterSurname} Sayfa Sayısı: {library2.PageCount} Yayınevi: {library2.Publisher} Kayıt Tarihi: {library2.RegistrationDate.ToString("yyyy-MM-dd")}"); // We print second object created with alternative constructor
